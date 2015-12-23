17be7572eadd031c34e50a4718d3abdec25da22b
New Feature Games <newfeaturegames@gmail.com>
2015-12-23 23:16:31 +0100

    Libgdx Test

---
+++ android/src/com/mvm/githubtest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mvm.githubtest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mvm.githubtest.Githubtest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Githubtest(), config);
+	}
+}
---
+++ desktop/src/com/mvm/githubtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mvm.githubtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mvm.githubtest.Githubtest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Githubtest(), config);
+	}
+}
